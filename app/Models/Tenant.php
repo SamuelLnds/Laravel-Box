adb506b5e85b4ee2bacf27395f6739b22e3f642d
SAMUEL LANDAIS <samuel.landais.pro@gmail.com>
2024-10-04 11:54:19 +0200

    correction #6 erreur au niveau du Model

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Tenant extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'phone_number',
+        'email',
+        'address',
+    ];
+
+    protected $casts = [
+        'email' => 'string',
+    ];
+
+    /**
+     * Get the user associated with the tenant.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Get the storage boxes associated with the tenant.
+     */
+    public function storageBoxes()
+    {
+        return $this->hasMany(StorageBox::class, 'tenant_id');
+    }
 }
